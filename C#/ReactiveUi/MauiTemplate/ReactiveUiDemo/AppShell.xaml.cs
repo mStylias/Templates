1bef6869ad4072d607c6b4efd43d691da8c01be3
StyleM <57811193+mStylias@users.noreply.github.com>
2022-10-28 23:43:52 +0300

    Add ReactiveUI Maui Template

---
+++ C#/ReactiveUi/MauiTemplate/ReactiveUiDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ReactiveUiDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ C#/ReactiveUi/MauiTemplate/ReactiveUiDemo/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace ReactiveUiDemo.ViewModels;
+
+public class MainViewModel : ReactiveObject
+{
+    [Reactive]
+    public string WelcomeMessage { get; set; }
+
+    public MainViewModel()
+    {
+        WelcomeMessage = "Welcome to .NET MAUI!";
+    }
+}
